42bf3931e9218d8b402d0d2225ba4976e39893d9
Gambizzz <anelisegambini@yahoo.fr>
2024-02-14 12:27:34 +0100

    first commit

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,40 @@
+class GossipsController < ApplicationController
+  def index
+    @gossips = Gossip.all
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    gossip = Gossip.create(post_params)
+    redirect_to gossip_path(gossip.id)
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    @gossip.update(post_params)
+    redirect_to basic_pages_home_path
+  end
+
+  def destroy
+    @gossip = GOSSIP.find(params[:id])
+    @gossip.destroy
+    redirect_to basic_pages_home_path
+  end
+
+  private
+  def post_params
+    post_params = params.require(:gossip).permit(:title, :content)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root 'basic_pages/#home'
+  
+  get 'basic_pages/home', to: 'basic_pages#home'
+  get 'basic_pages/team', to: 'basic_pages#team'
+  get 'basic_pages/contact', to: 'basic_pages#contact'
+  get 'basic_pages/welcome/:first_name', to: 'basic_pages#welcome'
+
+  resources :gossips
+ 
+
+
+  get "up" => "rails/health#show", as: :rails_health_check
+end
